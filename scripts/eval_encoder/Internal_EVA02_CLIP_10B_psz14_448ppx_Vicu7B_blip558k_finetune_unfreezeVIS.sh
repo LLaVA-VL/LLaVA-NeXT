2d20013f41c5a9d491954641b9e6309fc55388c6
Bo Li <drluodian@gmail.com>
2024-02-21 05:34:07 +0000

    add more specific scripts

--- scripts/eval_encoder/Internal_EVA02_CLIP_10B_psz14_448ppx_Vicu7B_blip558k_finetune_unfreezeVIS.sh
+++ scripts/eval_encoder/Internal_EVA02_CLIP_10B_psz14_448ppx_Vicu7B_blip558k_finetune_unfreezeVIS.sh
@@ -53,52 +53,12 @@ DATA_VERSION="blip558k"
 wandb login a651c244635bc6f913ab654af3f0eebaecdc9381
 wandb online
 
-RUN_NAME="llavanext-${MODEL_VERSION_CLEAN}-${VISION_MODEL_VERSION_CLEAN}-mlp2x_gelu-pretrain_${DATA_VERSION}_plain"
-echo "RUN_NAME: ${RUN_NAME}"
-deepspeed --include=localhost:$GPUS --master_port $PORT \
-    llava/train/train_mem.py \
-    --deepspeed chunyl_scripts/vc/train/ds_zero2.json \
-    --model_name_or_path ${MODEL_VERSION} \
-    --version ${PROMPT_VERSION} \
-    --data_path /mnt/bn/vl-research/data/llava/blip_6m/blip_558k_plain.json \
-    --image_folder /mnt/bn/vl-research/data/llava/blip_6m/images \
-    --vision_tower ${VISION_MODEL_VERSION} \
-    --vision_tower_pretrained ${VISION_MODEL_PRETRAINED} \
-    --tune_mm_mlp_adapter True \
-    --mm_vision_select_layer -2 \
-    --mm_use_im_start_end False \
-    --mm_use_im_patch_token False \
-    --group_by_modality_length True \
-    --image_aspect_ratio pad \
-    --bf16 True \
-    --output_dir ./project_checkpoints/${RUN_NAME} \
-    --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
-    --evaluation_strategy "no" \
-    --save_strategy "steps" \
-    --save_steps 5000 \
-    --save_total_limit 1 \
-    --learning_rate 1e-3 \
-    --weight_decay 0. \
-    --warmup_ratio 0.03 \
-    --lr_scheduler_type "cosine" \
-    --logging_steps 100 \
-    --tf32 True \
-    --model_max_length 4096 \
-    --gradient_checkpointing True \
-    --dataloader_num_workers 16 \
-    --lazy_preprocess True \
-    --report_to wandb \
-    --run_name ${RUN_NAME}
-
 PROMPT_VERSION="vicuna_v1"
 RUN_NAME="llavanext-${MODEL_VERSION_CLEAN}-${VISION_MODEL_VERSION_CLEAN}-mlp2x_gelu-pretrain_${DATA_VERSION}_${PROMPT_VERSION}_finetune_llava1.6_datamix_unfreezeVIS_1e"
 echo "RUN_NAME: ${RUN_NAME}"
 deepspeed --include=localhost:$GPUS --master_port $PORT \
     llava/train/train_mem.py \
-    --deepspeed chunyl_scripts/vc/train/ds_zero2.json \
+    --deepspeed chunyl_scripts/vc/train/ds_zero3.json \
     --model_name_or_path $MODEL_VERSION \
     --version $PROMPT_VERSION \
     --data_path ./playground/data/llava_instruct/llava_158k_detailv3_reinstall_gpt4v24k_wild15k_mixdocvqa_dca45k_synden40k_cococaps20k_sg40kt2k_ori.json \
