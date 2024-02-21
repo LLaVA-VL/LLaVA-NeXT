4977e1d166e5da1ff2a2fc372e932825f73529f8
Bo Li <drluodian@gmail.com>
2024-02-21 05:34:07 +0000

    add more specific scripts

--- scripts/eval_encoder/Internal_EVA02_CLIP_10B_psz14_Vicu7B_blip558k.sh
+++ scripts/eval_encoder/Internal_EVA02_CLIP_10B_psz14_Vicu7B_blip558k.sh
@@ -15,16 +15,32 @@ export HF_HUB_ENABLE_HF_TRANSFER="1"
 cd /mnt/bn/vl-research/workspace/boli01/projects/LLaVA_Next
 
 nvidia-smi
-PORT=26000
-GPUS="0,1,2,3,4,5,6,7"
+# 取 worker0 第一个 port
+ports=(`echo $METIS_WORKER_0_PORT | tr ',' ' '`)
+port=${ports[0]}
 
-# run experiment
+port_in_cmd="$(echo "${METIS_WORKER_0_PORT:-2222}"| awk -F',' '{print $1}')"
+
+echo "total workers: ${ARNOLD_WORKER_NUM}"
+echo "cur worker id: ${ARNOLD_ID}"
+echo "gpus per worker: ${ARNOLD_WORKER_GPU}"
+echo "master ip: ${METIS_WORKER_0_HOST}"
+echo "master port: ${port}"
+echo "master port in cmd: ${port_in_cmd}"
+
 export OMP_NUM_THREADS=8
 export NCCL_IB_DISABLE=0
 export NCCL_IB_GID_INDEX=3
 # export NCCL_IB_HCA=${ARNOLD_RDMA_DEVICE}
 export NCCL_SOCKET_IFNAME=eth0
 # export NCCL_DEBUG=INFO
+
+
+PORT=26000
+GPUS="0,1,2,3,4,5,6,7"
+
+wandb login a651c244635bc6f913ab654af3f0eebaecdc9381
+wandb online
 
 MODEL_VERSION="lmsys/vicuna-7b-v1.5"
 MODEL_VERSION_CLEAN="${MODEL_VERSION//\//_}"
@@ -38,7 +54,7 @@ DATA_VERSION="blip558k"
 # echo "RUN_NAME: ${RUN_NAME}"
 # deepspeed --include=localhost:$GPUS --master_port $PORT \
 #     llava/train/train_mem.py \
-#     --deepspeed chunyl_scripts/vc/train/ds_zero2.json \
+#     --deepspeed chunyl_scripts/vc/train/ds_zero3.json \
 #     --model_name_or_path ${MODEL_VERSION} \
 #     --version ${PROMPT_VERSION} \
 #     --data_path /mnt/bn/vl-research/data/llava/blip_6m/blip_558k_plain.json \
@@ -79,7 +95,7 @@ RUN_NAME="llavanext-${MODEL_VERSION_CLEAN}-${VISION_MODEL_VERSION_CLEAN}-mlp2x_g
 echo "RUN_NAME: ${RUN_NAME}"
 deepspeed --include=localhost:$GPUS --master_port $PORT \
     llava/train/train_mem.py \
-    --deepspeed chunyl_scripts/vc/train/ds_zero2.json \
+    --deepspeed chunyl_scripts/vc/train/ds_zero3.json \
     --model_name_or_path $MODEL_VERSION \
     --version $PROMPT_VERSION \
     --data_path ./playground/data/llava_instruct/llava_158k_detailv3_reinstall_gpt4v24k_wild15k_mixdocvqa_dca45k_synden40k_cococaps20k_sg40kt2k_ori.json \
