60803139b9b87cc8b7aa33fed3ba7b162e25de33
Bo Li <drluodian@gmail.com>
2024-02-29 09:47:01 +0000

    Refactor safe_save_model_for_hf_trainer function to only save mm_mlp_adapter and mm_vision_resampler if they are the only tunable parts

---
+++ playground/submit_jobs_sglang.sh
@@ -0,0 +1,19 @@
+export AZCOPY_CONCURRENCY_VALUE="AUTO"
+export HF_HOME=/mnt/bn/vl-research/workspace/boli01/.cache/huggingface
+export HF_TOKEN="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"
+export HF_HUB_ENABLE_HF_TRANSFER="1"
+
+python3 -m pip install --upgrade pip;
+
+python3 -m pip install "sglang[all]"
+
+cd /mnt/bn/vl-research/workspace/boli01/projects/sglang/python
+python3 -m pip install -e .
+
+pip install hf_transfer
+
+nvidia-smi
+
+python3 -m torch.utils.collect_env
+
+python3 -m sglang.launch_server --model-path liuhaotian/llava-v1.6-34b --tokenizer-path liuhaotian/llava-v1.6-34b-tokenizer --tp-size=8 --port=31215
