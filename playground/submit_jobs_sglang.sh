4c6ab347b0688dc24b1ed13035495b894f9d79c2
Bo Li <drluodian@gmail.com>
2024-03-01 04:17:00 +0000

    Fix pip install command and update pretrained model path

--- playground/submit_jobs_sglang.sh
+++ playground/submit_jobs_sglang.sh
@@ -3,7 +3,7 @@ export HF_HOME=/mnt/bn/vl-research/workspace/boli01/.cache/huggingface
 export HF_TOKEN="HF_Token"
 export HF_HUB_ENABLE_HF_TRANSFER="1"
 
-python3 -m pip install --upgrade pip;
+python3 -m pip install --upgrade pip
 
 python3 -m pip install "sglang[all]"
 
@@ -16,4 +16,4 @@ nvidia-smi
 
 python3 -m torch.utils.collect_env
 
-python3 -m sglang.launch_server --model-path liuhaotian/llava-v1.6-34b --tokenizer-path liuhaotian/llava-v1.6-34b-tokenizer --tp-size=8 --port=31215
+python3 -m sglang.launch_server --model-path liuhaotian/llava-v1.6-34b --tokenizer-path liuhaotian/llava-v1.6-34b-tokenizer
