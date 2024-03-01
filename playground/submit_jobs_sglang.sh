e762537353c791a9f3ee81d4378a3a466f2467ce
Bo Li <drluodian@gmail.com>
2024-03-01 16:16:32 +0000

    Update file paths and dependencies

--- playground/submit_jobs_sglang.sh
+++ playground/submit_jobs_sglang.sh
@@ -1,14 +1,18 @@
+#!/bin/bash
 export AZCOPY_CONCURRENCY_VALUE="AUTO"
 export HF_HOME=/mnt/bn/vl-research/workspace/boli01/.cache/huggingface
 export HF_TOKEN="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"
 export HF_HUB_ENABLE_HF_TRANSFER="1"
 
 cd /mnt/bn/vl-research/workspace/boli01/projects/sglang
-pip install --upgrade pip
-pip install -e "python[all]"
-pip install hf_transfer
+/home/tiger/miniconda3/bin/python3 -m pip install --upgrade pip
+/home/tiger/miniconda3/bin/python3 -m pip install -e "python[all]"
+/home/tiger/miniconda3/bin/python3 -m pip install hf_transfer
 
 nvidia-smi
 # python3 -m torch.utils.collect_env
 
-python3 -m sglang.launch_server --model-path liuhaotian/llava-v1.6-34b --tokenizer-path liuhaotian/llava-v1.6-34b-tokenizer --port=30000
+which python3
+
+cd /mnt/bn/vl-research/workspace/boli01/projects/sglang
+/home/tiger/miniconda3/bin/python3 -m sglang.launch_server --model-path liuhaotian/llava-v1.6-34b --tokenizer-path liuhaotian/llava-v1.6-34b-tokenizer --port=30000 --tp-size=8
