c92733d93c3a3b27b3ae799012059e2d444fd653
chunyuan.li <chunyuan.li@bytedance.com>
2024-03-03 00:54:07 +0800

    h800 gpu dev

--- playground/submit_jobs_sglang_on_merlin_cc3m.sh
+++ playground/submit_jobs_sglang_on_merlin_cc3m.sh
@@ -15,4 +15,4 @@ bash /mnt/bn/vl-research-cn-lf/workspace/projects/LLaVA_Next/playground/submit_j
 # Wait for a certain time, e.g., 10 seconds
 sleep 600;
 echo "Web service initialized";
-python /mnt/bn/vl-research-cn-lf/workspace/projects/LLaVA_Next/playground/sgl_llava_inference_multinode.py --image_folder=/mnt/bn/vl-research-cn-lf/data/llava_data/blip_558k/images --dist=0 --total_dist=8 --parallel=32 --port=30000
+python /mnt/bn/vl-research-cn-lf/workspace/projects/LLaVA_Next/playground/sgl_llava_inference_multinode.py --image_folder=/mnt/bn/vl-research-cn-lf/data/cc3m/images --result_file /mnt/bn/vl-research-cn-lf/workspace/projects/LLaVA_Next/playground/cc3m_llava34b_cap/cc3m_sglang_cap.json --dist=0 --total_dist=24 --parallel=32 --port=30000
