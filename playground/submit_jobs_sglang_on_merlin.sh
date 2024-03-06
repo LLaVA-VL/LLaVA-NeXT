29aecc5f812702d94495c6023c654a0aa23ad9e7
chunyuan.li <chunyuan.li@bytedance.com>
2024-03-06 14:17:58 +0800

    update

--- playground/submit_jobs_sglang_on_merlin.sh
+++ playground/submit_jobs_sglang_on_merlin.sh
@@ -29,7 +29,7 @@ nvidia-smi
 which python3
 
 cd /mnt/bn/${NAS_REGION}/workspace/projects/sglang
-/home/tiger/miniconda3/bin/python3 -m sglang.launch_server --model-path liuhaotian/llava-v1.6-34b --tokenizer-path liuhaotian/llava-v1.6-34b-tokenizer --port=30000 --tp-size=8
+/home/tiger/miniconda3/bin/python3 -m sglang.launch_server --model-path liuhaotian/llava-v1.6-34b --tokenizer-path liuhaotian/llava-v1.6-34b-tokenizer --port=30000 --tp-size=8 &
 
 sleep 480;
 echo "Web service initialized";
