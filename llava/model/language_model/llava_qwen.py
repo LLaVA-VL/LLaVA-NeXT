6879575554d384c9be7796474bcdf1670f090b18
Bo Li <drluodian@gmail.com>
2024-03-11 11:39:23 +0000

    updates

--- llava/model/language_model/llava_qwen.py
+++ llava/model/language_model/llava_qwen.py
@@ -47,7 +47,7 @@ class LlavaQwenForCausalLM(Qwen2ForCausalLM, LlavaMetaForCausalLM):
     def __init__(self, config):
         # super(Qwen2ForCausalLM, self).__init__(config)
         Qwen2ForCausalLM.__init__(self, config)
-        config.model_type = "llava_llama"
+        config.model_type = "llava_qwen"
         config.rope_scaling = None
         
         self.model = LlavaQwenModel(config)
