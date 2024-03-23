b9a28e2d0e5f89fb503e1e5ac4b9565b4678e282
Bo Li <drluodian@gmail.com>
2024-03-23 13:41:35 +0000

    Update image dimension calculation and plotting

--- scripts/data_info.py
+++ scripts/data_info.py
@@ -32,7 +32,7 @@ def calculate_image_dimension(item, images_folder):
         return None, None
 
 
-def calculate_image_dimensions_multiprocess(filtered_data, images_folder, num_processes=4):
+def calculate_image_dimensions_multiprocess(filtered_data, images_folder, num_processes=96):
     with Pool(num_processes) as p:
         dimensions = list(tqdm(p.imap(functools.partial(calculate_image_dimension, images_folder=images_folder), filtered_data), total=len(filtered_data), desc="Calculating image dimensions"))
     widths, heights = zip(*[dim for dim in dimensions if dim[0] is not None])
@@ -42,24 +42,24 @@ def calculate_image_dimensions_multiprocess(filtered_data, images_folder, num_pr
 def plot_histogram(data, title, xlabel, ylabel, bins_width=10):
     hist, bin_edges = np.histogram(data, bins=np.arange(0, max(data) + bins_width, bins_width))
     bins = np.arange(0, max(data) + 10, 10)  # Adjust the bin width as needed
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(12, 6))
     plt.bar(bin_edges[:-1], hist, width=bins_width - 1, edgecolor="black", log=True)
-    plt.xticks(np.arange(min(bins), max(bins) + 1, bins_width))
+    plt.xticks(np.arange(min(bins), max(bins) + 1, bins_width), rotation=90)
     plt.xlim(min(bin_edges), max(bin_edges))
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.title(title)
+    plt.title(title, fontsize=8)
     plt.savefig(f"./{title}.png")
     # plt.show()
 
 
 def plot_2d_histogram(widths, heights, title, xlabel, ylabel, bins_size=(500, 500)):  # Increased bin size
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(12, 12))
     h, xedges, yedges, image = plt.hist2d(widths, heights, bins=[np.arange(min(widths), max(widths) + bins_size[0], bins_size[0]), np.arange(min(heights), max(heights) + bins_size[1], bins_size[1])], cmap=plt.cm.jet, density=True)
     plt.colorbar()
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.title(title + "\n" + f"Max width: {max(widths)}, Max height: {max(heights)}")
+    plt.title(title + "\n" + f"Max width: {max(widths)}, Max height: {max(heights)}", fontsize=10)
     plt.savefig(f"./{title}.png")
 
 
@@ -97,7 +97,7 @@ def main():
     max_diwth = max(widths)
     max_height = max(heights)
     print(f"Max width: {max_diwth}, Max height: {max_height}")
-    plot_2d_histogram(widths, heights, f"{llava_instruct_name}_dist2d", "Width", "Height", bins_size=(300, 300))
+    plot_2d_histogram(widths, heights, f"dist_{llava_instruct_name}_2d_w_h", "Width", "Height", bins_size=(100, 100))
 
     tokenized_lengths = calculate_tokenized_lengths(filtered_data)
     plot_histogram(tokenized_lengths, f"dist_{llava_instruct_name}_tokenized_length", "Tokenized Length", "Count (log scale)", bins_width=10)
