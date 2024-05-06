1a493154f47c0f7ff8f8c0c39f7b4dbbbcd11cf8
kcz358 <kaichenzhang358@outlook.com>
2024-05-06 09:11:51 +0000

    Comment out print in conversation

--- llava/conversation.py
+++ llava/conversation.py
@@ -103,7 +103,7 @@ class Conversation:
                         message = "<image>" * len(images) + message
                     chat_template_messages.append({"role": role, "content": message})
 
-            print(chat_template_messages)
+            # print(chat_template_messages)
             return self.tokenizer.apply_chat_template(chat_template_messages, tokenize=False, add_generation_prompt=True)
             # ret = "" if self.system == "" else self.system + self.sep + "\n"
             # for role, message in messages:
