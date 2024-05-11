b46e496a98bd6bcf2b26fa6c28a675a6ea5cc228
Bo Li <bo.li01@bytedance.com>
2024-05-11 17:39:11 +0000

    Squashed commit of the following:
    commit cbc5654a2c4ac2528abba4ae06c03696c839c70d
    Author: Bo Li <bo.li01@bytedance.com>
    Date:   Sat May 11 17:35:20 2024 +0000
    
        updates
    
    commit 147a15df3f046299a85da2baa357ec1733aeb6ec
    Merge: 1b89104 de09d30
    Author: Bo Li <bo.li01@bytedance.com>
    Date:   Tue May 7 05:29:15 2024 +0000
    
        Merge branch 'bo/dev_decoder' of code.byted.org:ic-research/llava_next into bo/dev_decoder
    
    commit de09d3076997d8383bf7ede617a422366b529be5
    Merge: e763d7f 9ce838e
    Author: 李博 <bo.li01@bytedance.com>
    Date:   Tue May 7 05:29:09 2024 +0000
    
        Merge branch 'bo/dev_dpo' into 'bo/dev_decoder'
    
        [Feat] add mixtral to merge with active changes.
    
        See merge request ic-research/llava_next!14
    
    commit 9ce838ecb6dc31ececad2c289228413a02b5892f
    Author: 李博 <bo.li01@bytedance.com>
    Date:   Tue May 7 05:29:09 2024 +0000
    
        [Feat] add mixtral to merge with active changes.
    
        * Remove unused script and update dependencies
        * Update dependencies and add new scripts
        * Add list_data_dict initialization and update file paths for saving plots
        * Refactor DPODataset and add multimodal support to llava_mixtral
    
        https://code.byted.org/ic-research/llava_next/merge_requests/14
    
    commit 1b89104d378639a36ff01f1e7f34d4b30d36b838
    Author: Bo Li <bo.li01@bytedance.com>
    Date:   Tue May 7 05:29:06 2024 +0000
    
        update scripts

---
+++ scripts/cn_boli01_hl/CLIP336_Qwen110B_la1_6mix_ft_lr1e_5_ufvis_lr2e_6_filtered_improved_chat_data_v2_ep3.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+# set up wandb
+NAS_REGION="vl-research-cn-boli01-hl"
+USER_PROJECT="boli01"
+
+export WANDB_API_KEY=a651c244635bc6f913ab654af3f0eebaecdc9381
+export WANDB_ENTITY=llava-vl
+export WANDB_PROJECT=llava-next
+export WANDB_MODE=online
+export PYTHONWARNINGS="ignore"
+
+export ACCELERATE_DEBUG_MODE="1"
+export HF_HOME=/mnt/bn/${NAS_REGION}/workspace/.cache/huggingface
+export HF_TOKEN="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"
+export HF_HUB_ENABLE_HF_TRANSFER="1"
+
+export http_proxy=http://sys-proxy-rd-relay.byted.org:8118;
+export https_proxy=http://sys-proxy-rd-relay.byted.org:8118;
+
+############### Prepare Envs #################
+# set up llava dev env
+if [ -n "$USER_PROJECT" ]; then
+    cd /mnt/bn/${NAS_REGION}/workspace/${USER_PROJECT}/projects/LLaVA_Next
+else
+    cd /mnt/bn/${NAS_REGION}/workspace/projects/LLaVA_Next
+fi
+
+git config --global --add safe.directory '*'
+python3 -m pip install --upgrade pip;
+
+python3 -m pip install -e ".[train]"
+
+python3 -m pip install ninja
+python3 -m pip install flash-attn --no-build-isolation
+alias python=python3
+############### Show Envs ####################
+
+nvidia-smi
+# 取 worker0 第一个 port
+ports=($(echo $METIS_WORKER_0_PORT | tr ',' ' '))
+port=${ports[0]}
+port_in_cmd="$(echo "${METIS_WORKER_0_PORT:-2000}" | awk -F',' '{print $1}')"
+
+echo "total workers: ${ARNOLD_WORKER_NUM}"
+echo "cur worker id: ${ARNOLD_ID}"
+echo "gpus per worker: ${ARNOLD_WORKER_GPU}"
+echo "master ip: ${METIS_WORKER_0_HOST}"
+echo "master port: ${port}"
+echo "master port in cmd: ${port_in_cmd}"
+
+export OMP_NUM_THREADS=8
+export NCCL_IB_DISABLE=0
+export NCCL_IB_GID_INDEX=3
+# export NCCL_IB_HCA=${ARNOLD_RDMA_DEVICE}
+export NCCL_SOCKET_IFNAME=eth0
+export NCCL_DEBUG=INFO
+
+
+PORT=26000
+GPUS="0,1,2,3,4,5,6,7"
+
+wandb login a651c244635bc6f913ab654af3f0eebaecdc9381
+wandb online
+
+################ Arnold Jobs ################
+
+LLM_VERSION="Qwen/Qwen1.5-110B-Chat"
+LLM_VERSION_CLEAN="${LLM_VERSION//\//_}"
+VISION_MODEL_VERSION="openai/clip-vit-large-patch14-336"
+VISION_MODEL_VERSION_CLEAN="${VISION_MODEL_VERSION//\//_}"
+
+############### Pretrain ################
+
+PROMPT_VERSION=plain
+PRETRAIN_DATA_VERSION="blip558k"
+
+BASE_RUN_NAME="llavanext-${VISION_MODEL_VERSION_CLEAN}-${LLM_VERSION_CLEAN}-mlp2x_gelu-pretrain_blip558k_plain"
+echo "BASE_RUN_NAME: ${BASE_RUN_NAME}"
+
+PROMPT_VERSION="qwen_1_5"
+FINAL_RUN_NAME="llavanext-${VISION_MODEL_VERSION_CLEAN}-${LLM_VERSION_CLEAN}-ft_la1_6mix_lr1e_5_ufvis2e_6_anyres_d32k_filtered_data_add_chat_data_v2_ep3"
+echo "FINAL_RUN_NAME: ${FINAL_RUN_NAME}"
+ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node="${ARNOLD_WORKER_GPU}" --nnodes="${ARNOLD_WORKER_NUM}" --node_rank="${ARNOLD_ID}" --master_addr="${METIS_WORKER_0_HOST}" --master_port="${port_in_cmd}" \
+    llava/train/train_mem.py \
+    --deepspeed scripts/zero3.json \
+    --model_name_or_path ${LLM_VERSION} \
+    --version ${PROMPT_VERSION} \
+    --data_path="/mnt/bn/${NAS_REGION}/data/llava_instruct/{llava_158k_detailv3_reinstall_gpt4v24k_wild15k_mixdocvqa_dca45k_synden40k_cococaps20k_sg40kt2k_ori_rep_ai2d_filtered_794409,llava_wild_gpt4v_52190,real_vision_flan/sharegpt4v-coco-50k,real_vision_flan/sharegpt4v-knowledge-2k,real_vision_flan/sharegpt4v-llava-30k,real_vision_flan/sharegpt4v-sam-20k}.json" \
+    --image_folder /mnt/bn/${NAS_REGION}/data/llava_data \
+    --mm_tunable_parts="mm_vision_tower,mm_mlp_adapter,mm_language_model" \
+    --pretrain_mm_mlp_adapter="/mnt/bn/${NAS_REGION}/checkpoints/projectors/${BASE_RUN_NAME}/mm_projector.bin" \
+    --mm_vision_tower_lr=2e-6 \
+    --vision_tower ${VISION_MODEL_VERSION} \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --group_by_modality_length True \
+    --image_aspect_ratio anyres \
+    --image_grid_pinpoints "[(336, 672), (672, 336), (672, 672), (1008, 336), (336, 1008)]" \
+    --mm_patch_merge_type spatial_unpad \
+    --bf16 True \
+    --run_name $FINAL_RUN_NAME \
+    --output_dir /mnt/bn/${NAS_REGION}/checkpoints/$FINAL_RUN_NAME \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 20000 \
+    --save_total_limit 1 \
+    --learning_rate 1e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 32768 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 16 \
+    --lazy_preprocess True \
+    --report_to wandb
+
+exit 0;
