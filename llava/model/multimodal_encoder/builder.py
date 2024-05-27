bf6bb7f838bf5754a120c2f8209417b58364bee3
Bo Li <drluodian@gmail.com>
2024-05-27 09:19:23 +0000

    refactor(builder.py): comment out EvaClipVisionTower and EvaViTWrapper imports and usage feat(open_clip_encoder.py): import torchvision module to support OpenCLIP functionality

--- llava/model/multimodal_encoder/builder.py
+++ llava/model/multimodal_encoder/builder.py
@@ -2,11 +2,11 @@ import os
 from .clip_encoder import CLIPVisionTower
 from .imagebind import ImageBindWrapper
 from .open_clip_encoder import OpenCLIPVisionTower
-from .eva_clip.eva_clip_encoder import EvaClipVisionTower
-from .dev_eva_clip.eva_vit import EvaViTWrapper
 from .hf_vision import HFVisionTower
 from .siglip_encoder import SigLipVisionTower
 from .clip_encoder import CLIPVisionTower, CLIPVisionTowerS2
+# from .eva_clip.eva_clip_encoder import EvaClipVisionTower
+# from .dev_eva_clip.eva_vit import EvaViTWrapper
 
 
 def build_vision_tower(vision_tower_cfg, **kwargs):
@@ -20,15 +20,15 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
             return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
     elif "siglip" in vision_tower:
         return SigLipVisionTower(vision_tower, vision_tower_cfg=vision_tower_cfg, **kwargs)
-    elif "internal-eva" in vision_tower.lower() or "eva02" in vision_tower.lower():
-        return EvaClipVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
-    elif vision_tower in ["EVA-CLIP-8B", "EVA-CLIP-8B-plus"]:
-        return EvaViTWrapper(vision_tower, args=vision_tower_cfg, **kwargs)
     elif vision_tower.startswith("hf:"):
         return HFVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
     elif vision_tower in ["imagebind_huge"]:
         return ImageBindWrapper(vision_tower, args=vision_tower_cfg, **kwargs)
     elif vision_tower.startswith("open_clip_hub"):
         return OpenCLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+    # elif "internal-eva" in vision_tower.lower() or "eva02" in vision_tower.lower():
+    #     return EvaClipVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+    # elif vision_tower in ["EVA-CLIP-8B", "EVA-CLIP-8B-plus"]:
+    #     return EvaViTWrapper(vision_tower, args=vision_tower_cfg, **kwargs)
 
     raise ValueError(f"Unknown vision tower: {vision_tower}")
