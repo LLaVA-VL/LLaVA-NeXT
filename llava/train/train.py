0bfafd0f56e43a5e97541cf0982fed8deb99c35f
Bo Li <drluodian@gmail.com>
2024-07-14 01:20:46 +0000

    refactor: Update file loading logic in train.py and data_checker.py

--- llava/train/train.py
+++ llava/train/train.py
@@ -1040,6 +1040,7 @@ class LazySupervisedDataset(Dataset):
         length_list = []
         for sample in self.list_data_dict:
             cur_len = sum(len(conv["value"].split()) for conv in sample["conversations"])
+            assert cur_len > 0, f"Conversation length is 0 for {sample}"
             cur_len = cur_len if ("image" in sample) or ("video" in sample) else -cur_len
             length_list.append(cur_len)
         return length_list
