caa18d9690f493ef11e597f55497ea56b9b3e598
Bo Li <drluodian@gmail.com>
2024-08-06 13:02:13 +1000

    refactor: Update image size and conversation logic for generating text from images

--- playground/upload_data.py
+++ playground/upload_data.py
@@ -21,36 +21,36 @@ json_paths = [
     # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/textocr_gpt4v_train_converted_25114.json",
     # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/ai2d_train_internvl_single_12413.json",
     # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/textcaps_train_21952.json",
-    "/mnt/bn/vl-research/data/llava_instruct/ureader_new/ureader_qa_sft.json",
-    "/mnt/bn/vl-research/data/llava_instruct/ureader_new/ureader_cap_sft.json",
-    "/mnt/bn/vl-research/data/llava_instruct/ureader_new/ureader_ie_sft.json",
-    "/mnt/bn/vl-research/data/llava_instruct/ureader_new/ureader_kg_sft.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/vision_flan_filtered_186070.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/mathqa_29837.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/geo3k_2101.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/geo170k_qa_converted_67833.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/geo170k_align_converted_60252.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/sharegpt4v-coco-50k.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/sharegpt4v-knowledge-2k.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/sharegpt4v-llava-30k.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/sharegpt4v-sam-20k.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_CLEVR-Math_5290.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_FigureQA_17597.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_Geometry3K_9734.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_GeoQA+_17172.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_GEOS_508.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_IconQA_22599.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_MapQA_5235.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_PMC-VQA_35958.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_Super-CLEVR_8652.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_TabMWP_22462.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_UniGeo_11959.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_VizWiz_6614.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/magpie_pro_qwen2_72b_st_300000_sp_token_fltd_299992.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/magpie_pro_l3_80b_st_300000.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/magpie_pro_l3_80b_mt_300000_sp_token_fltd_299998.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/image_textualization_dataset_filtered.json",
-    "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/cambrian_filtered_gpt4vo_sp_token_fltd_max10k.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/ureader_new/ureader_qa_sft.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/ureader_new/ureader_cap_sft.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/ureader_new/ureader_ie_sft.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/ureader_new/ureader_kg_sft.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/vision_flan_filtered_186070.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/mathqa_29837.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/geo3k_2101.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/geo170k_qa_converted_67833.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/geo170k_align_converted_60252.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/sharegpt4v-coco-50k.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/sharegpt4v-knowledge-2k.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/sharegpt4v-llava-30k.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/sharegpt4v-sam-20k.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_CLEVR-Math_5290.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_FigureQA_17597.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_Geometry3K_9734.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_GeoQA+_17172.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_GEOS_508.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_IconQA_22599.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_MapQA_5235.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_PMC-VQA_35958.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_Super-CLEVR_8652.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_TabMWP_22462.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_UniGeo_11959.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/MathV360K_VizWiz_6614.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/magpie_pro_qwen2_72b_st_300000_sp_token_fltd_299992.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/magpie_pro_l3_80b_st_300000.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/magpie_pro_l3_80b_mt_300000_sp_token_fltd_299998.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/image_textualization_dataset_filtered.json",
+    # "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/cambrian_filtered_gpt4vo_sp_token_fltd_max10k.json",
     "/mnt/bn/vl-research/data/llava_instruct/real_vision_flan/sharegpt4o_dataset.jsonl",
     "/mnt/bn/vl-research/data/llava_instruct/cauldron/ai2d_llava_format_2434.json",
     "/mnt/bn/vl-research/data/llava_instruct/cauldron/aokvqa_16539_llava_format.json",
@@ -105,36 +105,36 @@ short_names = [
     # "textocr(gpt4v)",
     # "ai2d(internvl)",
     # "textcaps",
-    "ureader_qa",
-    "ureader_cap",
-    "ureader_ie",
-    "ureader_kg",
-    "vision_flan(filtered)",
-    "mathqa",
-    "geo3k",
-    "geo170k(qa)",
-    "geo170k(align)",
-    "sharegpt4v(coco)",
-    "sharegpt4v(knowledge)",
-    "sharegpt4v(llava)",
-    "sharegpt4v(sam)",
-    "CLEVR-Math(MathV360K)",
-    "FigureQA(MathV360K)",
-    "Geometry3K(MathV360K)",
-    "GeoQA+(MathV360K)",
-    "GEOS(MathV360K)",
-    "IconQA(MathV360K)",
-    "MapQA(MathV360K)",
-    "PMC-VQA(MathV360K)",
-    "Super-CLEVR(MathV360K)",
-    "TabMWP(MathV360K)",
-    "UniGeo(MathV360K)",
-    "VizWiz(MathV360K)",
-    "magpie_pro(qwen2_72b_st)",
-    "magpie_pro(l3_80b_st)",
-    "magpie_pro(l3_80b_mt)",
-    "image_textualization(filtered)",
-    "cambrian(filtered_gpt4vo)",
+    # "ureader_qa", # need to re-upload
+    # "ureader_cap", # need to re-upload
+    # "ureader_ie", # need to re-upload
+    # "ureader_kg", # need to re-upload
+    # "vision_flan(filtered)",
+    # "mathqa",
+    # "geo3k",
+    # "geo170k(qa)",
+    # "geo170k(align)",
+    # "sharegpt4v(coco)",
+    # "sharegpt4v(knowledge)",
+    # "sharegpt4v(llava)",
+    # "sharegpt4v(sam)",
+    # "CLEVR-Math(MathV360K)",
+    # "FigureQA(MathV360K)",
+    # "Geometry3K(MathV360K)",
+    # "GeoQA+(MathV360K)",
+    # "GEOS(MathV360K)",
+    # "IconQA(MathV360K)",
+    # "MapQA(MathV360K)",
+    # "PMC-VQA(MathV360K)",
+    # "Super-CLEVR(MathV360K)",
+    # "TabMWP(MathV360K)",
+    # "UniGeo(MathV360K)",
+    # "VizWiz(MathV360K)",
+    # "magpie_pro(qwen2_72b_st)",
+    # "magpie_pro(l3_80b_st)",
+    # "magpie_pro(l3_80b_mt)",
+    # "image_textualization(filtered)",
+    # "cambrian(filtered_gpt4vo)", # need to re-upload
     "sharegpt4o",
     "ai2d(cauldron,llava_format)",
     "aokvqa(cauldron,llava_format)",
@@ -174,31 +174,43 @@ short_names = [
 
 def upload_data(json_path, short_name):
     def gen():
-        with open(json_path, "r") as f:
-            data = json.load(f)
+        if json_path.endswith(".jsonl"):
+            with open(json_path, "r") as f:
+                data = [json.loads(line) for line in f]
+        else:
+            with open(json_path, "r") as f:
+                data = json.load(f)
 
-        preview_index = 10
+        preview_index = 5
         idx = 0
         for item in tqdm(data):
             if preview_index > 0:
                 preview_index -= 1
                 print(item)
                 continue
-            
-            image_path = f"/mnt/bn/vl-research/data/llava_data/{item['image']}"
+
             try:
-                with open(image_path, "rb") as img_file:
-                    image = pil_image.open(BytesIO(img_file.read()))
-            except:
-                print(f"Failed to load image {item['image']}")
+                if "image" in item:
+                    image_path = f"/mnt/bn/vl-research/data/llava_data/{item['image']}"
+                    try:
+                        with open(image_path, "rb") as img_file:
+                            image = pil_image.open(BytesIO(img_file.read()))
+                    except:
+                        print(f"Failed to load image {item['image']}")
+                        continue
+                else:
+                    image = None
+
+                item_id = item["id"] if "id" in item else f"{idx:06d}"
+                yield {"id": item_id, "image": image, "conversations": item["conversations"], "data_source": short_name}
+                idx += 1
+                
+            except Exception as e:
+                print(e)
                 continue
 
-            item_id = item["id"] if "id" in item else f"{idx:06d}"
-            yield {"id": item_id, "image": image, "conversations": item["conversations"], "data_source": short_name}
-            idx += 1
 
-
-    hf_dataset = Dataset.from_generator(generator=gen)
+    hf_dataset = Dataset.from_generator(generator=gen, num_proc=32)
     hf_dataset.push_to_hub("lmms-lab/LLaVA-OneVision-Data", config_name=short_name, split="train")
 
 for json_path, short_name in zip(json_paths, short_names):
