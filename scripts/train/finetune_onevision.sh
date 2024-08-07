76d219b77dbf5c1b36da0917eca2e766450396ee
kcz358 <kaichenzhang358@outlook.com>
2024-08-07 03:57:40 +0000

    Fix prompt version for training script

--- scripts/train/finetune_onevision.sh
+++ scripts/train/finetune_onevision.sh
@@ -13,7 +13,7 @@ VISION_MODEL_VERSION_CLEAN="${VISION_MODEL_VERSION//\//_}"
 
 ############### Pretrain ################
 
-PROMPT_VERSION=plain
+PROMPT_VERSION="qwen_1_5"
 
 BASE_RUN_NAME="llavanext-${VISION_MODEL_VERSION_CLEAN}-${LLM_VERSION_CLEAN}-mlp2x_gelu-pretrain_blip558k_plain"
 echo "BASE_RUN_NAME: ${BASE_RUN_NAME}"
