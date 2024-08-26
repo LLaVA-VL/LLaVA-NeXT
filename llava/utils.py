67a9b3986c41e6bb470e58055a29503f033add7e
ZhangYuanhan-AI <yuanhan002@ntu.edu.sg>
2024-08-26 07:02:21 +0000

    update video code

--- llava/utils.py
+++ llava/utils.py
@@ -25,18 +25,25 @@ except ImportError:
 def process_video_with_decord(video_file, data_args):
     vr = VideoReader(video_file, ctx=cpu(0), num_threads=1)
     total_frame_num = len(vr)
+    video_time = total_frame_num / vr.get_avg_fps()
     avg_fps = round(vr.get_avg_fps() / data_args.video_fps)
     frame_idx = [i for i in range(0, total_frame_num, avg_fps)]
+    frame_time = [i/avg_fps for i in frame_idx]
+
     
     if data_args.frames_upbound > 0:
-        if len(frame_idx) > data_args.frames_upbound:
+        if len(frame_idx) > data_args.frames_upbound or data_args.force_sample:
             uniform_sampled_frames = np.linspace(0, total_frame_num - 1, data_args.frames_upbound, dtype=int)
             frame_idx = uniform_sampled_frames.tolist()
+            frame_time = [i/vr.get_avg_fps() for i in frame_idx]
     
     video = vr.get_batch(frame_idx).asnumpy()
+    frame_time = ",".join([f"{i:.2f}s" for i in frame_time])
+
+    num_frames_to_sample = num_frames = len(frame_idx)
     # https://github.com/dmlc/decord/issues/208
     vr.seek(0)
-    return video
+    return video, video_time, frame_time, num_frames_to_sample
 
 def process_video_with_pyav(video_file, data_args):
     container = av.open(video_file)
