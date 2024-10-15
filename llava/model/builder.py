840fa78a716afe959cbcdadffd450005dfb215d2
ShacklesLay <shackleslay@gmail.com>
2024-10-15 14:34:39 +0800

    fix resize bugs

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -306,9 +306,10 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
         mm_use_im_patch_token = getattr(model.config, "mm_use_im_patch_token", True)
         if mm_use_im_patch_token:
             tokenizer.add_tokens([DEFAULT_IMAGE_PATCH_TOKEN], special_tokens=True)
+            model.resize_token_embeddings(len(tokenizer))
         if mm_use_im_start_end:
             tokenizer.add_tokens([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True)
-        model.resize_token_embeddings(len(tokenizer))
+            model.resize_token_embeddings(len(tokenizer))
 
         vision_tower = model.get_vision_tower()
         if not vision_tower.is_loaded:
@@ -608,9 +609,10 @@ def load_pretrained_model_wo_low_cpu(model_path, model_base, model_name, load_8b
         mm_use_im_patch_token = getattr(model.config, "mm_use_im_patch_token", True)
         if mm_use_im_patch_token:
             tokenizer.add_tokens([DEFAULT_IMAGE_PATCH_TOKEN], special_tokens=True)
+            model.resize_token_embeddings(len(tokenizer))
         if mm_use_im_start_end:
             tokenizer.add_tokens([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True)
-        model.resize_token_embeddings(len(tokenizer))
+            model.resize_token_embeddings(len(tokenizer))
 
         vision_tower = model.get_vision_tower()
         if not vision_tower.is_loaded:
