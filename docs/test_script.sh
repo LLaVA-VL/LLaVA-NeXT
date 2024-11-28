f703a362e1664c0742cc280f697fa94f0f57da99
Ziqin Zhang <ziqin.zhang@tum.de>
2024-11-28 23:55:49 +0100

    batch script added

---
+++ docs/test_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH --gres=gpu:1
+#SBATCH --time=01:00:00
+#SBATCH --job-name=testjob_llava
+#SBATCH --export=NONE
+
+unset SLURM_EXPORT_ENV
+
+module load python
+conda activate llava
+
+python3 video_tutorial.py
