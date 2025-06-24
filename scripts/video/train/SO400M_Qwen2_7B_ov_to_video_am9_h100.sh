2e88f1699b08271a9cbce028c9e80452ec8918c1
hyperfraise <damien.menigaux@veesion.io>
2025-06-24 17:24:01 +0200

    Set batch size to 1 per device for faster training startup
    - Change per_device_train_batch_size from 8 to 1
    - Change per_device_eval_batch_size from 8 to 1
    - This reduces effective batch size from 64 to 8 (8 GPUs × 1)
    - Training will start after processing only 8 samples instead of 64

--- scripts/video/train/SO400M_Qwen2_7B_ov_to_video_am9_h100.sh
+++ scripts/video/train/SO400M_Qwen2_7B_ov_to_video_am9_h100.sh
@@ -110,8 +110,8 @@ ACCELERATE_CPU_AFFINITY=1 torchrun \
     --run_name $MID_RUN_NAME \
     --output_dir ./work_dirs/$MID_RUN_NAME \
     --num_train_epochs 5 \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 8 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "steps" \
     --eval_steps 500 \
